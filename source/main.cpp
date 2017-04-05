8c18f967b4438e8d7b59581a92a68d37b9644e76
kracejic <kracejic@gmail.com>
2017-04-04 22:56:35 -0500

    working

--- source/main.cpp
+++ source/main.cpp
@@ -20,6 +20,7 @@ using Connections = std::set<websocketpp::connection_hdl,
     std::owner_less<websocketpp::connection_hdl>>;
 
 Connections connections;
+// TODO mutex
 
 
 void on_message(websocketpp::connection_hdl hdl, server::message_ptr msg)
@@ -27,6 +28,12 @@ void on_message(websocketpp::connection_hdl hdl, server::message_ptr msg)
     std::cout << msg->get_payload() << std::endl;
 
     webSocketServer.send(hdl, "test", websocketpp::frame::opcode::text);
+
+    if (msg->get_payload() == "exit")
+    {
+        connections.clear();
+        webSocketServer.stop();
+    }
 }
 //-----------------------------------------------------------------------------
 void on_open(websocketpp::connection_hdl hdl)
@@ -44,37 +51,56 @@ void on_close(websocketpp::connection_hdl hdl)
 int main(int argc, char** argv)
 {
     po::parser parser;
-    parser["optimization"].abbreviation('O').type(po::u32).fallback(
-        0); // if --optimization is not explicitly specified, assume 0
 
-    parser["include-path"] // corresponds to --include-path
-        .abbreviation('I') // corresponds to -I
-        .type(po::string)  // expects a string
-        .multi();          // allows multiple arguments for the same option
+    parser["delay"].abbreviation('d').type(po::u32).fallback(100).description(
+        "Delay between messages");
+    parser["port"].abbreviation('p').type(po::u32).fallback(9000);
+    parser["verbose"].abbreviation('v').description("Verbose output");
+
+    parser["help"]
+        .abbreviation('?')
+        .abbreviation('h')
+        .description("print this help screen")
+        .callback([&] { std::cout << parser << '\n'; });
 
     parser(argc, argv);
+    if (parser["help"].size())
+        return 0;
+
+    int delay = parser["delay"].get().u32;
 
     webSocketServer.set_message_handler(&on_message);
     webSocketServer.set_open_handler(&on_open);
     webSocketServer.set_close_handler(&on_close);
     webSocketServer.set_listen_backlog(64);
 
+    if (not parser["verbose"].size())
+        webSocketServer.clear_access_channels(websocketpp::log::alevel::all);
+
 
     webSocketServer.init_asio();
-    webSocketServer.listen(9004);
+    webSocketServer.listen(parser["port"].get().u32);
     webSocketServer.start_accept();
 
-    // async(std::launch::async, []() {
-    //     // webSocketServer.websocketpp::transport::asio::basic_socket::endpoint::is_secure
-    //     while (true)
-    //     {
-    //         sleep(1);
-    //         for (auto& con : connections)
-    //         {
-    //             webSocketServer.send(con, "test", websocketpp::frame::opcode::text);
-    //         }
-    //     }
-    // });
+    bool done = false;
+    auto sendThread = async(std::launch::async, [delay, &done]() {
+        while (not done)
+        {
+            cout << "sending batch" << endl;
+            usleep(delay * 1000);
+            for (auto& con : connections)
+            {
+                cout << "  * data:" << endl;
+                webSocketServer.send(
+                    con, "test", websocketpp::frame::opcode::text);
+            }
+        }
+    });
 
+    cout << "Starting server" << endl;
     webSocketServer.run();
+    done = true;
+    cout << "Stopping sending thread" << endl;
+    sendThread.get();
+    cout << "Sending thread stopped" << endl;
 }
